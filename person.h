7835d06e08d62e0ea741dc2906b9d6e06bd35bc9
Albert Cieslak <albert.cieslak2@gmail.com>
2018-01-10 00:50:40 +0100

    wiecej sie kompiluje

--- person.h
+++ person.h
@@ -2,6 +2,7 @@
 #define JNP1_CHRISTMASS_TREE_PERSON_CC_H
 
 #include <string>
+#include <memory>
 #include "helper.h"
 
 class Person {
@@ -15,8 +16,8 @@ private:
 public:
     std::string getName();
     Adult(std::string name,Price savings = Price(0) );
-//    void buyDecoration(Decoration& decoration);
     Price getSavings();
+    void buyDecoration(const Decoration& decoration);
 
 };
 
