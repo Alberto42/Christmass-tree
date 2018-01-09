349f97897487ebfae20c09feadff999e19d2f160
Albert Cieslak <albert.cieslak2@gmail.com>
2018-01-09 18:46:55 +0100

    initial

---
+++ helper.cc
@@ -0,0 +1,64 @@
+#include "helper.h"
+#include <iostream>
+#include <random>
+
+int randomNumber() {
+    std::mt19937 gen(0);
+    std::uniform_int_distribution<> dis(0, std::numeric_limits<int>::max());
+    return dis(gen);
+}
+
+Price Price::operator+(const Price &p) const {
+    return Price(amount + p.amount);
+}
+
+Price& Price::operator-=(const Price &p) {
+    amount -= p.amount;;
+    return *this;
+}
+
+bool Price::operator<(const Price &p) const {
+    return amount < p.amount;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Price &p) {
+    return stream << p.amount << " PLN";
+}
+
+
+Date& Date::operator++() {
+    if (minute < 59) minute++;
+    else {
+        minute = 0;
+        if (hour < 23) hour++;
+        else {
+            hour = 0;
+            day++;
+        }
+    }
+    return *this;
+}
+
+int operator-(const Date &d1, const Date &d2) {
+    return d1.minute - d2.minute + (d1.hour - d2.hour) * 60 + 
+           (d1.day - d2.day) * 60 * 24;
+}
+
+bool Date::operator<(const Date &d) const {
+    if (day != d.day) return day < d.day;
+    else if (hour != d.hour) return hour < d.hour;
+    else return minute < d.minute;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Date &d) {
+    return stream << "December, " << d.day << ", " << d.hour << ":";
+    if(d.minute < 10) stream << "0";
+    stream << d.minute;
+    return stream;
+}
+
+
+Date& currentDate() {
+    static Date current(18, 16, 0);
+    return current;
+}
---
+++ helper.h
@@ -0,0 +1,32 @@
+#ifndef HELPER
+#define HELPER
+
+#include <iostream>
+
+int randomNumber();
+
+class Price {
+    int amount;
+public:
+    Price(int a = 0) : amount(a) {}
+    Price operator+(const Price &p) const;
+    Price& operator-=(const Price &p);
+    bool operator<(const Price &p) const;
+    friend std::ostream& operator<<(std::ostream& stream, const Price &p);
+};
+
+class Date {
+    int day;
+    int hour;
+    int minute;
+public:
+    Date(int d, int h, int m) : day(d), hour(h), minute(m) {}
+    Date& operator++();
+    friend int operator-(const Date &d1, const Date &d2);
+    bool operator<(const Date &d) const;
+    friend std::ostream& operator<<(std::ostream& stream, const Date &d);
+};
+
+Date& currentDate();
+
+#endif //HELPER
